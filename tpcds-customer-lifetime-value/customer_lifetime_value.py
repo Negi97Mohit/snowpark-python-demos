55d8d504d8d5e9541a376e24aea43a17b8ef8415
Negi97Mohit <mohit.snegi97@gmail.com>
2023-03-15 17:35:19 -0400

    Updated Aprt-2 for mid term

--- tpcds-customer-lifetime-value/customer_lifetime_value.py
+++ tpcds-customer-lifetime-value/customer_lifetime_value.py
@@ -60,6 +60,7 @@
 #         budgets.append(budget)
 
 import pandas as pd
+import numpy as np
 from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 import streamlit as st
@@ -73,15 +74,69 @@ from py_scripts import part1 as p1
 st.set_page_config(layout="wide")       
 st.title("MId-Term Assignment")
 
+def run_query(dob_list,education_option,gender_option,dept_option,credit_option,marital_option):
+    # if len(education_option)!=0 and len(gender_option)!=0 and len(dept_option)!=0 and len(credit_option)!=0:
+    #     query='''select sum(prediction) Predicted,sum(actual_sales) Actual_Sales
+    #             from predictions
+    #             where c_birth_year between '''+ str(dob_list[0]) +''' and '''+str( dob_list[1])+''' 
+    #             and cd_gender in'''+ str(gender_option)+'''
+    #             and cd_marital_status in'''+str(marital_option)+''' 
+    #             and cd_dep_count in '''+str(dept_option)+'''
+    #             and cd_credit_rating in '''+ str(credit_option)+'''
+    #             and cd_education_status in '''+ str(education_option)
+        
+    # elif len(education_option)==1 and len(gender_option)==1 and len(dept_option)==1 and len(credit_option)==1:
+    #     education_option=list(education_option)
+    #     gender_option=list(gender_option)
+    #     dept_option=list(dept_option)
+    #     credit_option=list(credit_option)
+    #     marital_option=list(marital_option)
+    #     query='''select sum(prediction) Predicted,sum(actual_sales) Actual_Sales
+    #             from predictions
+    #             where c_birth_year between '''+ str(dob_list[0]) +''' and '''+str( dob_list[1])+''' 
+    #             and cd_gender in('''+ str(gender_option[0])+''')
+    #             and cd_marital_status in ('''+str(marital_option[0])+''') 
+    #             and cd_dep_count in ('''+str(dept_option[0])+''')
+    #             and cd_credit_rating in ('''+ str(credit_option[0])+''')'''
+
+    # else:
+    #     query='''select sum(prediction) Predicted,sum(actual_sales) Actual_Sales
+    #             from predictions
+    #             where c_birth_year between '''+ str(dob_list[0]) +''' and '''+str( dob_list[1]) 
+
+    query_vals=['''
+    and cd_gender in'''+ str(gender_option),
+            '''
+            and cd_marital_status in'''+str(marital_option), 
+        '''
+        and cd_dep_count in '''+str(dept_option),
+        '''
+        and cd_credit_rating in '''+ str(credit_option),
+        '''
+        and cd_education_status in '''+ str(education_option)]
+    str_pos=[True,True,True,True,True]
+    if len(gender_option)==0:str_pos[0]=False
+    if len(marital_option)==0:str_pos[1]=False
+    if len(dept_option)==0:str_pos[2]=False
+    if len(credit_option)==0:str_pos[3]=False
+    if len(education_option)==0:str_pos[4]=False
+
+    query_prime='''select sum(prediction) Predicted,sum(actual_sales) Actual_Sales
+                from predictions
+                where c_birth_year between '''+ str(dob_list[0]) +''' and '''+str( dob_list[1])
+    for post_stat,query_val in zip(str_pos,query_vals):
+        if post_stat==True:
+            query_prime=query_prime+query_val
+
+    return query_prime
+
 def main():
-    
     with st.container():
         st.header("Part - 1")
         st_part1()
     with st.container():
-        st.header("Part - 2")
+        st.header("Part - 2: Customer Lifetime Valuation using XGBoost")
         st_part2()
-        st.write("Hello World")
 
 def st_part1():
     col1, col2 = st.columns(2,gap='small')
@@ -98,7 +153,6 @@ def st_part1():
             quality='high',
             key='Car'
         )
-
         with st.expander("View Main Code"):
             p1code=inspect.getsource(p1)
             st.code(p1code,language='python')
@@ -135,7 +189,57 @@ def st_part1():
             p1.part1()
 
 def st_part2():
-    pass
+    part2_engine = create_engine(URL(
+            account = 'ieimkyf-atb67363',
+            user = 'MOHITSNEGI123',
+            password = 'Hollyhalston97)',
+            database = 'TPCDS_XGBOOST',
+            schema = 'DEMO',
+            warehouse = 'COMPUTE_WH',
+            role='ACCOUNTADMIN',
+        ))
+    connection=part2_engine.connect()
+    
+    col1,col2=st.columns(2)
+    with col1:
+        dob_slider=st.slider('Date of Birth',1924,1992,(1924,1992))
+        dob_list=list(dob_slider)
+
+        education_option = st.multiselect(
+            'Select Education Level',
+            ['Advanced Degree','Secondary','2 yr Degree','4 yr Degree','College','Unknown','Primary'],
+            ['Advanced Degree','Secondary','2 yr Degree'])
+
+        gender_option=st.multiselect(
+            'Select Customer Demographic by Sex',
+            ['M','F'],
+            ['F']
+        )
+
+        dept_option=st.multiselect(
+            'Select Customer Dept',
+            ['0','1'],
+            ['0']
+        )
+
+        credit_option=st.multiselect(
+            'Select Credit Rating',
+            ['Low Risk',' Good', 'High Risk', 'Unknown'],
+            ['Low Risk',' Good']
+        )
+
+        marital_option=st.multiselect(
+            'Select Marital Status',
+            ['S','M','W','U','D'],
+            ['W','U']
+        )
+        
+    with col2:
+        query=run_query(list(dob_list),tuple(education_option),tuple(gender_option),tuple(dept_option),tuple(credit_option),tuple(marital_option))
+        st.code(query,language='SQL')
+        res2=pd.read_sql(query,connection)
+        st.write(res2)
+    
 
 if __name__=="__main__":
     main()
